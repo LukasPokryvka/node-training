bc52ef42b100dc1832eb7fa943e81889499129d2
LukasPokryvka <58267318+LukasPokryvka@users.noreply.github.com>
2021-11-15 15:25:39 +0100

    added router

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const app = express()
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(express.json())
 app.use(
 	express.urlencoded({
@@ -9,19 +12,7 @@ app.use(
 	})
 )
 
-app.use('/add-product', (req, res, next) => {
-	res.send(
-		'<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-	)
-})
-
-app.post('/product', (req, res, next) => {
-	console.log(req.body)
-	res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-	res.send('<h1>Hello from express</h1>')
-})
+app.use(adminRoutes)
+app.use(shopRoutes)
 
 app.listen(3000)
